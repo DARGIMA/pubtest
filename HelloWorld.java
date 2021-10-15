534ba0ea39243882df8c634939b9d06ac0b1fc82
DARGIMA <47316416+DARGIMA@users.noreply.github.com>
2021-10-15 23:21:51 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! Jenkinx"); 
+        System.out.println("Hello, World! Jenkinx1"); 
     }
 }
